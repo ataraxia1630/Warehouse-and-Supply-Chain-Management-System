aa402a3b0b0a7ba313e17225b6d670aa49815afe
VidIsWandering <tomnguyen023@gmail.com>
2025-09-26 20:49:08 +0700

    config exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './config/logger.config';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger });
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
