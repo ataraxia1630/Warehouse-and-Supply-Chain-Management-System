e126907866c67be360a183a0de25eb7ce9f00c35
VidIsWandering <tomnguyen023@gmail.com>
2025-09-26 20:58:12 +0700

    config validation pipe for API I/O

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './config/logger.config';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger });
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,        
+      forbidNonWhitelisted: true, 
+      transform: true,        
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
