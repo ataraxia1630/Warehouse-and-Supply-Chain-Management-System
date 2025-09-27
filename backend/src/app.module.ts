6300b98b8c5e10081f330f532128e2d89b547049
VidIsWandering <tomnguyen023@gmail.com>
2025-09-27 21:38:41 +0700

    chore(app): register modular monolith modules and DB modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { validationSchema } from './config/validation';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
+
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { OrderModule } from './modules/order/order.module';
 import { SupplierModule } from './modules/supplier/supplier.module';
 import { ReportingModule } from './modules/reporting/reporting.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -17,6 +19,7 @@ import { ReportingModule } from './modules/reporting/reporting.module';
       load: [configuration],
       validationSchema,
     }),
+    DatabaseModule,
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
