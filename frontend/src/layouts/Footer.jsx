7be59aff950b889c4800e7b4341ae6887fc507b2
nhittyn <hudinhdong@gmail.com>
2025-09-28 12:57:05 +0700

    feat: add initial components, pages, and utility functions

---
+++ frontend/src/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      © 2025 Warehouse and Supply Chain Management System. All rights reserved.
+    </div>
+  );
+}
