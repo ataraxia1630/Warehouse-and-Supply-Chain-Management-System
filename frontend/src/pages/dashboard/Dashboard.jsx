c26cfd2c0e086f332381065e83108a3fca421848
nhittyn <hudinhdong@gmail.com>
2025-10-09 22:02:36 +0700

    test: test layout with long text

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,137 @@
 import React from 'react';
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        sollicitudin malesuada eros, non suscipit metus aliquet id. Donec eu
+        arcu ut risus facilisis vulputate. Integer vehicula, lectus nec
+        tincidunt eleifend, purus tortor eleifend nisl, nec tempor lorem nisi ac
+        purus. Suspendisse pretium tincidunt risus, nec tristique orci bibendum
+        vel. Sed sit amet leo euismod, varius magna at, finibus libero. Cras vel
+        lectus eu odio tincidunt imperdiet. Quisque id sapien nec tortor
+        fringilla egestas. Vivamus euismod, orci nec luctus vestibulum, justo
+        mauris consequat magna, id porttitor metus ipsum vitae libero. Integer
+        aliquet lacus ut augue bibendum ultricies. Donec malesuada, justo eget
+        suscipit mattis, eros magna dapibus massa, nec tempor tortor est id
+        ante. Pellentesque luctus turpis sed sem volutpat, vel suscipit enim
+        sagittis. Suspendisse varius, lorem ac lacinia mattis, urna nisi mollis
+        enim, ac malesuada ex metus ac ipsum. Nam fermentum lacus ut convallis
+        vulputate. Proin sagittis lorem sed vestibulum sodales. Suspendisse
+        scelerisque in justo ac mattis.
+      </p>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?
+      </p>
+
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        et quas molestias excepturi sint occaecati cupiditate non provident,
+        similique sunt in culpa qui officia deserunt mollitia animi, id est
+        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+        voluptatibus maiores alias consequatur aut perferendis doloribus
+        asperiores repellat.
+      </p>
+
+      <p>
+        Donec tristique, lectus vel gravida dictum, mi ex sodales erat, sed
+        porttitor lorem turpis at justo. Sed ut fermentum sem. In vitae metus
+        nec purus tincidunt aliquam. Nulla facilisi. Vivamus cursus luctus erat,
+        at sodales tellus imperdiet id. Sed consequat, justo sit amet pulvinar
+        aliquet, justo sapien luctus ipsum, id ullamcorper sem augue a orci. Nam
+        et tincidunt libero. Aenean interdum justo vitae semper accumsan. Duis
+        vel sapien id lacus vulputate malesuada. Aliquam erat volutpat.
+        Suspendisse faucibus lacinia nunc, nec pretium risus malesuada in.
+        Integer eget urna non leo condimentum aliquam. Suspendisse non augue
+        erat. Nam suscipit odio non enim fermentum ornare. Curabitur fringilla
+        risus ac orci blandit, nec egestas nisi vulputate.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        sollicitudin malesuada eros, non suscipit metus aliquet id. Donec eu
+        arcu ut risus facilisis vulputate. Integer vehicula, lectus nec
+        tincidunt eleifend, purus tortor eleifend nisl, nec tempor lorem nisi ac
+        purus. Suspendisse pretium tincidunt risus, nec tristique orci bibendum
+        vel. Sed sit amet leo euismod, varius magna at, finibus libero. Cras vel
+        lectus eu odio tincidunt imperdiet. Quisque id sapien nec tortor
+        fringilla egestas. Vivamus euismod, orci nec luctus vestibulum, justo
+        mauris consequat magna, id porttitor metus ipsum vitae libero. Integer
+        aliquet lacus ut augue bibendum ultricies. Donec malesuada, justo eget
+        suscipit mattis, eros magna dapibus massa, nec tempor tortor est id
+        ante. Pellentesque luctus turpis sed sem volutpat, vel suscipit enim
+        sagittis. Suspendisse varius, lorem ac lacinia mattis, urna nisi mollis
+        enim, ac malesuada ex metus ac ipsum. Nam fermentum lacus ut convallis
+        vulputate. Proin sagittis lorem sed vestibulum sodales. Suspendisse
+        scelerisque in justo ac mattis.
+      </p>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?
+      </p>
+
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        et quas molestias excepturi sint occaecati cupiditate non provident,
+        similique sunt in culpa qui officia deserunt mollitia animi, id est
+        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+        voluptatibus maiores alias consequatur aut perferendis doloribus
+        asperiores repellat.
+      </p>
+
+      <p>
+        Donec tristique, lectus vel gravida dictum, mi ex sodales erat, sed
+        porttitor lorem turpis at justo. Sed ut fermentum sem. In vitae metus
+        nec purus tincidunt aliquam. Nulla facilisi. Vivamus cursus luctus erat,
+        at sodales tellus imperdiet id. Sed consequat, justo sit amet pulvinar
+        aliquet, justo sapien luctus ipsum, id ullamcorper sem augue a orci. Nam
+        et tincidunt libero. Aenean interdum justo vitae semper accumsan. Duis
+        vel sapien id lacus vulputate malesuada. Aliquam erat volutpat.
+        Suspendisse faucibus lacinia nunc, nec pretium risus malesuada in.
+        Integer eget urna non leo condimentum aliquam. Suspendisse non augue
+        erat. Nam suscipit odio non enim fermentum ornare. Curabitur fringilla
+        risus ac orci blandit, nec egestas nisi vulputate.
+      </p>
+    </div>
+  );
 }
