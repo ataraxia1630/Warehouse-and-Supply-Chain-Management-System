a38fdbb0097f324a0c8f8e159ba381c222d8fd8e
nhittyn <hudinhdong@gmail.com>
2025-10-09 21:06:09 +0700

    feat: implement sidebar and header components

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import { Box, IconButton, Avatar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export default function Header() {
-  return <div>Header</div>;
+export default function Header({ height = 60 }) {
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height,
+        bgcolor: '#ffffff',
+        position: 'fixed',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        padding: '0 24px',
+        borderBottom: '1px solid #e0e0e0',
+        top: '0',
+      }}
+    >
+      <Avatar
+        alt="User Avatar"
+        src="/path/to/avatar.jpg"
+        sx={{
+          width: '40px',
+          height: '40px',
+          '&:hover': {
+            cursor: 'pointer',
+            boxShadow: '0 0 8px rgba(0, 0, 0, 0.2)',
+          },
+        }}
+        onClick={() => navigate('/profile')}
+      />
+    </Box>
+  );
 }
--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,18 +1,29 @@
 import Header from './Header';
 import Footer from './Footer';
+import Sidebar from '@components/Sidebar';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
+  const SIDEBAR_WIDTH = 208;
+  const HEADER_HEIGHT = 48;
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
-    >
-      <Header />
-      <main style={{ flex: 1 }}>
-        {/* Outlet = chỗ hiển thị page con */}
-        <Outlet />
-      </main>
-      <Footer />
+    <div style={{ minHeight: '100vh' }}>
+      <Header height={HEADER_HEIGHT} />
+      <div style={{ display: 'flex' }}>
+        <Sidebar width={SIDEBAR_WIDTH} headerHeight={HEADER_HEIGHT + 1} />
+        <main
+          style={{
+            flex: 1,
+            marginLeft: SIDEBAR_WIDTH, // chừa chỗ cho sidebar
+            marginTop: HEADER_HEIGHT, // chừa chỗ cho header
+            padding: 16,
+            minHeight: `calc(100vh - ${HEADER_HEIGHT}px)`,
+          }}
+        >
+          <Outlet />
+        </main>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
